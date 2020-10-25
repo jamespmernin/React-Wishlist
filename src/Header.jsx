7b5ebb1208b3eec05611efe8d5d13fc2f379e45e
James Mernin <jamespmernin@gmail.com>
2020-10-25 11:41:07 -0400

    Touch-ups

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,9 +6,9 @@ function Header() {
   return (
     <div className="header">
       <h1 className="title">Wishlist App</h1>
-      <nav >
-        <h2 className="nav-items">Lists</h2>
-        <h2 className="nav-items">Sign Up</h2>
+      <nav>
+        <h2 className="nav">Lists</h2>
+        <h2 className="nav">Sign Up</h2>
       </nav>
     </div>
   )
